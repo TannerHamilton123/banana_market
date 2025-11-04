f2d3108a84d1b89fc8995e5300189c9a92590f4c
TannerHamilton <155474958+TannerHamilton123@users.noreply.github.com>
2025-11-04 12:53:46 -0800

    Refactor selling logic and price determination

--- scripts/farmer.gd
+++ scripts/farmer.gd
@@ -5,11 +5,12 @@ var sold : bool = false
 var sell : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Global.market.connect("banana_sold","banana_to_market
 	$time_to_sell.set_wait_time(randf_range(1,2))
 	pass # Replace with function body.
 
 func determine_price():
-	if sold or not sell:
+	if sold:
 		price = price + randf_range(0,10)
 	else:
 		price = price - randf_range(0,10)
@@ -21,7 +22,7 @@ func banana_to_market(banana_price):
 	banana.seller = self
 	Global.market.add_banana(banana)
 
-func supply_algorithm(banana_price):
+func decide_to_sell(banana_price):
 	var willing_to_sell = banana_price
 	var chance = randi() % 100
 	if chance < willing_to_sell:
@@ -31,7 +32,7 @@ func supply_algorithm(banana_price):
 
 func _on_time_to_sell_timeout() -> void:
 	determine_price()
-	supply_algorithm(price)
+	decide_to_sell(price)
 	if sell:
 		banana_to_market(price)
 
